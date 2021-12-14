081bd1c39f226db31869bd3c1ec4f2b04834270e
Eric Zimmerman <saericzimmerman@gmail.com>
2021-12-13 21:40:55 -0500

    modernize sln

--- VSCMount/Properties/AssemblyInfo.cs
+++ VSCMount/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("VSCMount")]
-[assembly: AssemblyDescription("Mount VSCs easily")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VSCMount")]
-[assembly: AssemblyProduct("VSCMount")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("Eric Zimmerman")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a67d58ca-a29a-418f-9419-e8e4c5a70edb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
